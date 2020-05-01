c485c7285d300aff24c21000c44f4d11cc814582
João Prates <jvdprates@hotmail.com>
2020-05-01 19:15:36 -0300

    Tá andando mas tá devagar

--- knexfile.js
+++ knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
         connection: {
             host: process.env.MYSQL_HOST || 'localhost',
             user: process.env.MYSQL_USER || 'root',
-            password: process.env.MYSQL_PASSWORD || 'cpejr123',
+            password: process.env.MYSQL_PASSWORD || 'password',
             database: process.env.MYSQL_DATABASE || 'bemconectado'
         },
         migrations: {
