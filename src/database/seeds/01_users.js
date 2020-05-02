c3c90effd0dfb4b943dbba42245aff9025582b11
João Prates <jvdprates@hotmail.com>
2020-05-01 21:09:32 -0300

    Testando seeds

--- src/database/seeds/01_users.js
+++ src/database/seeds/01_users.js
@@ -1,10 +1,10 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('table_name').del()
+  return knex('users').del()
     .then(function () {
       // Inserts seed entries
-      return knex('table_name').insert([
+      return knex('users').insert([
         {name: 'Jose', email: 'jose@hotmail.com', firebase: 'KHSBAHKNDJAHXHA'},
         {name: 'Maria', email: 'Maria@hotmail.com', firebase: 'KHSBAHFKAJDJAHXHA'},
         {name: 'Paula', email: 'Paula@hotmail.com', firebase: 'KHSBAHKABCJNAKAHXHA'},
