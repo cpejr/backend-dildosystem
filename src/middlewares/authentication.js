405478c1232e2fe9b759ac2d755732fc505f8fd9
Giovanna <giovanna.pimenta11@gmail.com>
2020-05-18 16:48:10 -0300

    middleware de token opcional

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -34,5 +34,35 @@ module.exports = {
     else {
       next();
     }
-  }
+  },
+
+  async authenticateOptionalToken(request, response, next) {
+    const authHeader = request.headers.authorization;
+    const [scheme, token] = authHeader
+      ? authHeader.split(" ")
+      : [undefined, undefined];
+
+    if (!token && !scheme)
+      return next();
+
+    if (!token || token === null)
+      return response.status(401).json({ error: "No token provided" });
+
+    if (!/^Bearer$/i.test(scheme))
+      return response.status(401).json({ error: "Token badformatted" });
+    
+
+    const validToken = await new Promise((res) => {
+      jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if (err) return res(false);
+        
+        request.session = user;
+
+        return res(true);
+      });
+    });
+
+    if (validToken) return next();
+    return response.status(403).json({ error: "Invalid authorization token" });
+  },
 }
