12bd97d7b166b1322aad80bd556e20bb8aaa4036
Giovanna <giovanna.pimenta11@gmail.com>
2020-05-19 20:37:10 -0300

    Tabela orders + modificações

--- src/database/migrations/20200519190105_creating_orders.js
+++ src/database/migrations/20200519190105_creating_orders.js
@@ -1,23 +1,31 @@
 
-exports.up = function(knex) {
-  return knex.schema.createTable('orders', function (table){
+exports.up = function (knex) {
+  return knex.schema.createTable('orders', function (table) {
     table.increments();
-    table.integer('client_id').notNullable();
-    table.foreign('client_id').references('id').inTable('clients').onDelete('SET NULL');
+    table.integer('user_id').notNullable();
+    table.foreign('user_id').references('id').inTable('users').onDelete('SET NULL');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.string('payment_type').nullable();
-    table.enu('status', ['pending','paid','mailed']).notNullable();
+    table.enu('status', ['pending', 'paid', 'mailed']).notNullable();
   })
-  .createTable('orders_products', function(table){
-    table.integer('order_id').notNullable();
-    table.foreign('order_id').references('id').inTable('orders').onDelete('CASCADE');
-    table.integer('product_id').nullable();
-    table.foreign('product_id').references('id').inTable('products').onDelete('SET NULL');
-    table.integer('product_quantity').unsigned().notNullable();
-  });
+    .createTable('orders_products', function (table) {
+      table.integer('order_id').notNullable();
+      table.foreign('order_id').references('id').inTable('orders').onDelete('CASCADE');
+      table.integer('product_id').nullable();
+      table.foreign('product_id').references('id').inTable('products').onDelete('SET NULL');
+      table.integer('product_quantity').unsigned().notNullable();
+      table.integer('subproduct_id').nullable();
+      table.foreign('subproduct_id').references('id').inTable('subproducts').onDelete('SET NULL');
+    })
+    .table('products', function (table) {
+      table.integer('stock_quantity').notNullable().defaultTo(0).unsigned();
+    });
 };
 
-exports.down = function(knex) {
-    return knex.schema.dropTable('orders_products')
+exports.down = function (knex) {
+  return knex.schema.table('products', function(table){
+    table.dropColumn('stock_quantity');
+  })
+    .dropTable('orders_products')
     .dropTable('orders');
 };
