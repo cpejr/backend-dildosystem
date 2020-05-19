bfae648fd30ee78a05eeca3aca7c575091e0ee4b
joao pedro <jpmm.brasil@hotmail.com>
2020-05-19 15:13:53 -0300

    tabela de subprodutos

--- src/database/connection.js
+++ src/database/connection.js
@@ -2,4 +2,6 @@ const environment = process.env.NODE_ENV || 'development';
 const configuration = require('../../knexfile')[environment];
 console.log(configuration);
 
-module.exports = require('knex')(configuration);
+const connection = require('knex')(configuration);
+connection.raw("PRAGMA foreign_keys = ON");
+module.exports = connection;
---
+++ src/database/migrations/20200519142631_adding_subproducts_and_editing_products.js
@@ -0,0 +1,29 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('subproducts', function (table) {
+        table.increments();
+        table.string('name').notNullable();
+        table.string('description').nullable();
+        table.boolean('visible').notNullable().defaultTo(false);
+        table.int('stock_quantity').notNullable();
+        table.string('image_id').nullable();
+        table.int('product_id').notNullable();
+        table.foreign('product_id').references('id').inTable('products').onDelete('CASCADE');
+        table.timestamp('created_at').defaultTo(knex.fn.now());
+        table.timestamp('updated_at').defaultTo(knex.fn.now());
+    })
+    .table('products', function (table) {   
+        table.boolean('has_subproducts').notNullable().defaultTo(false);
+        table.renameColumn('wholesailer_price', 'wholesaler_price');
+        table.renameColumn('wholesailer_sale_price', 'wholesaler_sale_price');
+    });
+};
+
+exports.down = function(knex) {
+    return knex.schema.dropTable('subproducts')
+    .table('products', function (table) {   
+        table.dropColumn('has_subproducts');
+        table.renameColumn('wholesaler_price', 'wholesailer_price');
+        table.renameColumn('wholesaler_sale_price', 'wholesailer_sale_price');
+    });
+};
