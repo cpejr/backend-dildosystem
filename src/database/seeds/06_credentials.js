a3edd3914dd287b8af029438415f96f2b68af3a1
Ian <ian.wanderleyx@gmail.com>
2020-10-06 23:36:21 -0300

    Correção das credentials

--- src/database/seeds/06_credentials.js
+++ src/database/seeds/06_credentials.js
@@ -7,8 +7,8 @@ exports.seed = function(knex) {
       return knex('credentials').insert([
         
             {
-              "access_token" : "ya29.a0AfH6SMC70MsDo4IkTK97vZi3TdV66CCAykq70mycxoNle6QH9O9jjDaa-phigc-75xg_YUUuR-gyC3uaP6ZJfOV8FiMa6tqocSCrSWhekZuIjextJgJYoM9qIalESHWmDvZtIcexfj68P4-jxB3EcD61fyej48wJG5c",
-              "refresh_token" : "1\/\/0heeHs4QZVYgACgYIARAAGBESNwF-L9IrWZfxTC8yMg3yKDz1Jr8PJXTQRHu6KRx3-yQSbpYvgDkJrhUVrJ0mlZZAKa8TUXU4rJ4",
+              "access_token" : 'ya29.a0AfH6SMB-zCXh10FEx_6dLBd3hEKKcyFdsF1ecqYVxEgXg1FBocKJJqaT_pNW58BbUw4yyN_NUM0CspPf-PZy-RCHTs-gJdUKh1CbEhyJDljlyiNrkHB1x6LyCYAGKdRRQJ4cM4eLbtNbV_oW9Hkx28jsFy4CMMHxh_c',
+              "refresh_token" : '1//0hKWPCwxRWMm_CgYIARAAGBESNwF-L9IrAM1bsE0LebkoaZg9qlmp36s5G22flEcBfej7ZestQCyAoKwV1nCMQ29cPTE2DQ_Vztk',
               "scope" : "https:\/\/www.googleapis.com\/auth\/drive",
               "token_type" : "Bearer",
               "expiry_date" : 1598924145866
