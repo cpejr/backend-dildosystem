c4f5fa6819bdc6e00fcff3ad4c1665ba0805e226
Elias <eliasfaria@ufmg.br>
2020-10-23 18:39:17 -0300

    Fix: Consertando ordem dos migration e tipo errado de id

--- src/database/migrations/20200519190105_creating_orders.js
+++ src/database/migrations/20200519190105_creating_orders.js
@@ -2,9 +2,9 @@
 exports.up = function (knex) {
   return knex.schema.createTable('orders', function (table) {
     table.string('id').primary().notNullable();
-    table.integer('user_id').notNullable();
+    table.string('user_id').notNullable();
     table.foreign('user_id').references('id').inTable('users').onDelete('SET NULL');
-    table.integer('address_id').notNullable();
+    table.string('address_id').notNullable();
     table.foreign('address_id').references('id').inTable('address').onDelete('SET NULL');
     table.timestamp('created_at').defaultTo(knex.fn.now());
     table.string('payment_type').nullable();
