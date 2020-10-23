43a885bad2f4a06c21034c148cae9b5a504f0090
Elias <eliasfaria@ufmg.br>
2020-10-23 18:08:19 -0300

    Fix: Configurando banco de dados postgres

--- knexfile.js
+++ knexfile.js
@@ -19,18 +19,23 @@ module.exports = {
   },
 
   production: {
-    client: 'mysql',
+    client: 'pg',
     connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
+      host: process.env.DB_HOST,
+      database: process.env.DB_NAME,
+      user:     process.env.DB_USER,
+      password: process.env.DB_PASS
     },
+
     pool: {
       min: 2,
       max: 10
     },
     migrations: {
-      tableName: 'knex_migrations'
+      directory: './src/database/migrations'
+    },
+    seeds: {
+      directory: './src/database/seeds'
     },
     pool: {
       afterCreate: (conn, cb) =>
