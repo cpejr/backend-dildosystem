5c747b1d4d789d9e6935f86fa14141a6bba4ddc4
Vitor <vmarquesds@gmail.com>
2020-11-25 19:50:01 -0300

    Feat: back para o banner images V1

---
+++ src/database/migrations/20201123161858_banner_table.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('banner', function (table) {
+    table.string('id').primary().notNullable();
+    table.string('image_id').notNullable();
+    table.integer('position').notNullable();
+});
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTable('banner');
+};
